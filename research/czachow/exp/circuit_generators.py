134677f42cf89ff4ba7386e85b95fb4e3632f264
Christopher Zachow <christopher.zachow@sva.de>
2021-03-19 09:56:51 +0100

    changed parameters for exp0 and created new circuit with half measurement

--- research/czachow/exp/circuit_generators.py
+++ research/czachow/exp/circuit_generators.py
@@ -2,21 +2,39 @@ import numpy
 from qiskit.circuit.library import GraphState
 
 
-def graphstate_manhatten_ring(ring_size=12):
+def graphstate_manhatten_ring(num_qubits=12):
     def ring_map(size):
         for j in range(size):
-            yield [j, (j+1) % ring_size]
-            yield [(j+1) % ring_size, j]
+            yield [j, (j+1) % size]
+            yield [(j+1) % size, j]
 
-    coupling_map = list(ring_map(ring_size))
+    coupling_map = list(ring_map(num_qubits))
 
     rows, cols = zip(*coupling_map)
 
-    matrix = numpy.zeros((ring_size, ring_size))
+    matrix = numpy.zeros((num_qubits, num_qubits))
     matrix[rows, cols] = 1
 
     qc = GraphState(matrix)
+
+    return qc
+
+
+def graphstate_manhatten_ring_fm(num_qubits=12):
+    qc = graphstate_manhatten_ring(num_qubits)
     qc.measure_all()
+    return qc
+
+
+def graphstate_manhatten_ring_hm(num_qubits=12):
+    qc = graphstate_manhatten_ring(num_qubits)
+
+    creg = qc._create_creg(int(num_qubits / 2), 'meas')
+    qc.add_register(creg)
+    qc.barrier()
+    for j in range(num_qubits):
+        if j % 2 == 0:
+            qc.measure(j, int(j / 2))
 
     return qc
 
@@ -25,6 +43,24 @@ def graphstate_complete(num_qubits):
 
     matrix = numpy.ones((num_qubits, num_qubits))
     qc = GraphState(matrix)
+
+    return qc
+
+
+def graphstate_complete_fm(num_qubits):
+    qc = graphstate_complete(num_qubits)
     qc.measure_all()
+    return qc
+
+
+def graphstate_complete_hm(num_qubits):
+    qc = graphstate_complete(num_qubits)
+
+    creg = qc._create_creg(int(num_qubits / 2), 'meas')
+    qc.add_register(creg)
+    qc.barrier()
+    for j in range(num_qubits):
+        if j % 2 == 0:
+            qc.measure(j, int(j / 2))
 
     return qc
--- research/czachow/exp/custom_passmanager.py
+++ research/czachow/exp/custom_passmanager.py
@@ -11,7 +11,7 @@ from qiskit.transpiler.passes import ApplyLayout
 from qiskit.circuit.equivalence_library import SessionEquivalenceLibrary as sel
 
 
-def custom_pass_manager(backend, layout_instance, seed=None):
+def custom_pass_manager(backend, layout_instance, layout_reps=1, seed=None):
     # basis_gates = backend.configuration().basis_gates
     basis_gates = ['s', 'sdg', 'sx', 'h', 'cx']
     coupling_map = CouplingMap(backend.configuration().coupling_map)
@@ -19,6 +19,7 @@ def custom_pass_manager(backend, layout_instance, seed=None):
     def _swap_condition(property_set):
         return not property_set['is_swap_mapped']
 
+    _layout = [layout_instance] * layout_reps
     _embed = [FullAncillaAllocation(coupling_map), EnlargeWithAncilla(), ApplyLayout()]
     _unroll3q = Unroll3qOrMore()
     _swap_check = CheckMap(coupling_map)
@@ -27,7 +28,7 @@ def custom_pass_manager(backend, layout_instance, seed=None):
     _unroll = [UnrollCustomDefinitions(sel, basis_gates), BasisTranslator(sel, basis_gates)]
 
     pm = PassManager()
-    pm.append(layout_instance)
+    pm.append(_layout)
     pm.append(_embed)
     pm.append(_unroll3q)
     pm.append(_swap_check)
