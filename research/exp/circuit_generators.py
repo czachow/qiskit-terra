8e4922769f905eb1e9ccc3df1e7c9c55afd5cb10
Christopher Zachow <christopher.zachow@sva.de>
2021-07-19 10:16:48 +0200

    created 12q corner case and ran counter

--- research/exp/circuit_generators.py
+++ research/exp/circuit_generators.py
@@ -3,13 +3,22 @@ from qiskit.circuit.library import GraphState
 from qiskit.circuit.quantumcircuit import QuantumCircuit
 
 
-def graphstate_manhattan_ring(num_qubits=12):
-    def ring_map(size):
+def graphstate_ring(num_qubits=12):
+    """
+    x - x - x - x
+    |           |
+    x           x
+    |           |
+    x           x
+    |           |
+    x - x - x - x
+    """
+    def cpl_map(size):
         for j in range(size):
             yield [j, (j+1) % size]
             yield [(j+1) % size, j]
 
-    coupling_map = list(ring_map(num_qubits))
+    coupling_map = list(cpl_map(num_qubits))
     rows, cols = zip(*coupling_map)
 
     matrix = numpy.zeros((num_qubits, num_qubits))
@@ -24,22 +33,35 @@ def graphstate_manhattan_ring(num_qubits=12):
 
     return qc
 
+def graphstate_ring_corners(num_qubits=12):
+    """
+    x - x - x - x
+    | /       \ |
+    x           x
+    |           |
+    x           x
+    | \       / |
+    x - x - x - x
+    """
+    def cpl_map(size):
+        for j in range(size):
+            yield [j, (j+1) % size]
+            yield [(j+1) % size, j]
+            if j % 3 == 2:
+                yield [j, (j+2) % size]
+                yield [(j+2) % size, j]
+    coupling_map = list(cpl_map(num_qubits))
 
-def graphstate_manhattan_ring_fm(num_qubits=12):
-    qc = graphstate_manhattan_ring(num_qubits)
-    qc.measure_all()
-    return qc
+    rows, cols = zip(*coupling_map)
+    matrix = numpy.zeros((num_qubits, num_qubits))
+    matrix[rows, cols] = 1
 
-
-def graphstate_manhattan_ring_hm(num_qubits=12):
-    qc = graphstate_manhattan_ring(num_qubits)
-
-    creg = qc._create_creg(int(num_qubits / 2), 'meas')
-    qc.add_register(creg)
-    qc.barrier()
-    for j in range(num_qubits):
-        if j % 2 == 0:
-            qc.measure(j, int(j / 2))
+    qc = QuantumCircuit(num_qubits)
+    qc.h(range(num_qubits))
+    for i in range(num_qubits):
+        for j in range(i + 1, num_qubits):
+            if matrix[i][j] == 1:
+                qc.cz(i, j)
 
     return qc
 
@@ -58,20 +80,18 @@ def graphstate_complete(num_qubits):
     return qc
 
 
-def graphstate_complete_fm(num_qubits):
-    qc = graphstate_complete(num_qubits)
-    qc.measure_all()
-    return qc
+def circuit_add_meas(circuit, meas_type):
 
+    # half measurement
+    if meas_type == "hm":
+        creg = circuit._create_creg(int(circuit.num_qubits / 2), 'meas')
+        circuit.add_register(creg)
+        circuit.barrier()
+        for j in range(circuit.num_qubits):
+            if j % 2 == 0:
+                circuit.measure(j, int(j / 2))
 
-def graphstate_complete_hm(num_qubits):
-    qc = graphstate_complete(num_qubits)
-
-    creg = qc._create_creg(int(num_qubits / 2), 'meas')
-    qc.add_register(creg)
-    qc.barrier()
-    for j in range(num_qubits):
-        if j % 2 == 0:
-            qc.measure(j, int(j / 2))
-
-    return qc
+    elif meas_type == "fm":
+        circuit.measure_all()
+    
+    return circuit
